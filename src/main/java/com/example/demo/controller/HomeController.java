7516788ca873d60982771a3ede117ab806745702
rajcocvs <32100208+rajcocvs@users.noreply.github.com>
2025-06-20 07:56:46 +0530

    Update HomeController.java
    mycanada

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
   
     @GetMapping({"/", "/home"})
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to My Spring Boot Web Application NCPL!");
+        model.addAttribute("message", "Welcome to My Spring Boot Web Application NCPL CANADA!");
         return "home";
     }
 }
